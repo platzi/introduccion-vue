a23048fa02b6b932cd769436cf41750c23d03003
Diana Martinez <diananerdoficial@gmail.com>
2022-02-02 18:05:15 -0600

    Vue CLI service

---
+++ Hola.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+    <p>Lorem ipsum dolor</p>
+    <div>{{ text }}</div>
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            text: "Hola!"
+        };
+    }
+};
+</script>
+
+<style>
+p {
+    color: red;
+}
+</style>
